1e789b5ddb99ede82c10c689b81a0b7615a2d16a
IT21235092 <99312102+IT21235092@users.noreply.github.com>
2022-05-20 01:19:13 +0530

    Create Visitor.h

---
+++ Visitor.h
@@ -0,0 +1,19 @@
+#pragma once
+class Visitor
+{
+    protected :
+            int ID;
+            char fullName[50];
+            char address[50];
+            int mobile;
+            char email[20];
+            
+    public :
+            Visitor();
+            Visitor(int V_ID, const char Name[], const char adrs[], int phone, const char V_email[]);
+            void login();
+            void registerUser();
+            void displayDetails();
+            ~Visitor();
+            
+};
