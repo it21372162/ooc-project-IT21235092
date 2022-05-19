a3e6e594bebc2fef3d2324ee40940c6376075dd5
IT21235092 <99312102+IT21235092@users.noreply.github.com>
2022-05-20 00:25:44 +0530

    Create Customer.h

---
+++ Customer.h
@@ -0,0 +1,30 @@
+#pragma once
+#include "Visitor.h"
+#include "Package.h"
+#include "Booking.h"
+#include "Feedback.h"
+
+
+#define SIZE 5
+
+class Customer : public Visitor
+{
+    protected :
+            char Username[30];
+            char Password[15];
+            
+            Package *pkg[SIZE];
+            Booking *bk[SIZE];
+            Feedback *fb[SIZE];
+    
+    public :
+            Customer();
+            Customer(const char C_name[], const char C_pass[], int V_email_ID, const char Name[], const char adrs[], int phone, const char V_email[]);
+            void login();
+            void makePayement();
+            void placeBooking(Booking *bking);
+            void searchPackages(Package *pk);
+            void addFeedbacks(Feedback *feed);
+            ~Customer();
+            
+};
