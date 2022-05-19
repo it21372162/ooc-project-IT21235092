c40163317a0088de43d764a5fe0697a5813e208d
it21372162 <99360801+it21372162@users.noreply.github.com>
2022-05-20 01:34:00 +0530

    Create Report.cpp

---
+++ Report.cpp
@@ -0,0 +1,33 @@
+#include "Report.h"
+#include <cstring>
+
+Report :: Report()
+{
+  
+}
+
+Report :: Report(int R_ID)
+{
+    reportID=R_ID;
+}
+           
+void Report :: generateBookDetails(Booking *R_bk)
+{
+
+  
+}
+
+void Report :: generatePaymentDetails(Payment *R_pay)
+{
+  
+}
+
+void Report :: generateConfirmationDetails()
+{
+  
+}
+
+Report :: ~Report()
+{
+  
+}
